2ef3dd3477321c806122f1903048cf0ea5ac89fb
Rei Rigos <mcrei1018@gmail.com>
2024-03-16 20:07:21 +0800

    First commit

--- Program 2.py
+++ Program 2.py
@@ -4,5 +4,5 @@ font = Figlet(font="epic")
 your_name = str(input("Enter Your Name: "))
 your_dream_job = str(input("Enter Your Dream Job: "))
 
-textArt = (font.renderText(your_name + " " + your_dream_job))
-print('\033[95m' + textArt)
+text_art = (font.renderText(your_name + " " + your_dream_job))
+print('\033[95m' + text_art)
