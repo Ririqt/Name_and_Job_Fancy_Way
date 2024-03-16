150a95dd5d13ba1c49e214182ec2ffb5343b0000
Mharc Reinald Rigos <131633325+Ririqt@users.noreply.github.com>
2024-03-17 00:15:22 +0800

    Update Program 2.py

--- Program 2.py
+++ Program 2.py
@@ -2,7 +2,7 @@ from pyfiglet import Figlet
 
 font = Figlet(font="epic")
 your_name = str(input("Enter Your Name: "))
-your_dream_job = str(input("Enter Your Dream Job: "))
+your_dream_job = str(input("Enter Your Name: "))
 
 text_art = (font.renderText(your_name + " " + your_dream_job))
 print('\033[95m' + text_art)
